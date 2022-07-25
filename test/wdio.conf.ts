2e8013216b0236633c12538f08cac3f120707a5b
Marcin Mazurek <marcin.mazurek@iohk.io>
2022-07-25 16:44:13 +0200

    Fix webdriver config

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -6,56 +6,44 @@ const fs = require('fs');
 //  build: { daedalus },
 //} = packageJson;
 
+const isInDebugMode = process.env.DEBUG === 'true' || process.argv.includes('--debug');
+
 export const config = {
-  // ...
+  services: [
+    [
+      'electron',
+      {
+        appPath: join(__dirname, 'dist'),
+        appName: 'daedalus',
+        appArgs: ['foo', 'bar=baz'],
+        chromedriver: {
+          port: 9519,
+          logFileName: 'wdio-chromedriver.log',
+        },
+      },
+    ],
+  ],
+  capabilities: [{}],
+  waitforTimeout: 5000,
+  connectionRetryCount: 10,
+  connectionRetryTimeout: 30000,
+  logLevel: isInDebugMode ? 'debug' : 'warn',
+  runner: 'local',
+  outputDir: process.env.CI === 'true' ? 'daedalus' : undefined,
+  specs: ['./test/specs/**/*.e2e.ts'],
   autoCompileOpts: {
     autoCompile: true,
-    // see https://github.com/TypeStrong/ts-node#cli-and-programmatic-options
-    // for all available options
     tsNodeOpts: {
       transpileOnly: true,
-      project: 'tsconfig.json'
+      files: true,
+      project: join(__dirname, 'test', 'tsconfig.json'),
     },
-    services: [
-      [
-        'electron',
-        {
-          appPath: join(__dirname, 'dist'),
-          appName: 'daedalus',
-          appArgs: ['foo', 'bar=baz'],
-          chromedriver: {
-            port: 9519,
-            logFileName: 'wdio-chromedriver.log',
-          },
-        },
-      ],
-    ],
-    waitforTimeout: 5000,
-    connectionRetryCount: 10,
-    connectionRetryTimeout: 30000,
-    logLevel: 'debug',
-    runner: 'local',
-    outputDir: process.env.CI === 'true' ? 'daedalus' : undefined,
-    specs: ['test/specs/**.ts',
-    ['test/specs/**']],
-    autoCompileOpts: {
-      autoCompile: true,
-      tsNodeOpts: {
-        transpileOnly: true,
-        files: true,
-        project: join(__dirname, 'tsconfig.json'),
-      },
-    },
-    framework: 'mocha',
-    mochaOpts: {
-      ui: 'bdd',
-      timeout: 30000,
-    },
-    // tsconfig-paths is only used if "tsConfigPathsOpts" are provided, if you
-    // do please make sure "tsconfig-paths" is installed as dependency
-    tsConfigPathsOpts: {
-      baseUrl: './'
-    }
-  }
-}
+  },
+  framework: 'mocha',
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 30000,
+  },
+};
+
 module.exports = { config };
